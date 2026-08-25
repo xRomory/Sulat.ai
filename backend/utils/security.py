dd17eea7e41cda4d1fc95778440adee3842ddaaa
chayen <romory@pop-os.CG-24D>
2026-08-25 14:56:57 +0800

    Cleaned security.py

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,7 +1,4 @@
-from passlib.context import CryptContext
 import bcrypt
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_password_hash(password: str) -> str:
     password_bytes = password.encode("utf-8")
